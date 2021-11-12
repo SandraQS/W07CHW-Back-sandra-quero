43ef5b618b64a80d61bbe813adae08cbd7e4d5c6
SandraQS <sanquesan@gmail.com>
2021-11-12 20:42:02 +0100

    Created /register and /login at userRoutes

---
+++ server/routes/usersRoutes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.post("/register", (req, res, next) => {
+  res.json("register");
+});
+
+router.post("/login", (req, res, next) => {
+  res.json("login");
+});
+
+module.exports = router;
