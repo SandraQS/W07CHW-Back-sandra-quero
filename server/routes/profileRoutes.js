1ecc6a9116c321b7388f854a99e03b2208aab01e
SandraQS <sanquesan@gmail.com>
2021-11-13 20:15:58 +0100

    Created getFriends controller

--- server/routes/profileRoutes.js
+++ server/routes/profileRoutes.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const auth = require("../middlewares/auth");
-const { getUsers, addFriends } = require("../controllers/profileController");
+const {
+  getUsers,
+  addFriends,
+  getFriends,
+} = require("../controllers/profileController");
 
 const router = express.Router();
 
 router.get("/", auth, getUsers);
 router.post("/friends", auth, addFriends);
+router.get("/friends", auth, getFriends);
 
 module.exports = router;
